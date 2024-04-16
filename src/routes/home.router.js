6fafc1f5ab3126cd425b4613b68374fb620968fd
ciro <giorginiciro@gmail.com>
2024-04-16 17:12:44 -0300

    carrito en hbs

--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -16,6 +16,32 @@ router.get('/home', async (req, res) => {
     })
 })
 
+router.get('/cart/:cid', async (req, res) => {
+    try {
+        const cartId = req.params.cid
+    
+        const CartManager = req.app.get('cartManager')
+        const productToCart = await CartManager.getCartById(cartId)
+
+        const products = productToCart.products.map(d => d.toObject({ virtuals: true }))
+        console.log(productToCart)
+
+        res.render('cart', {
+            title: 'Carrito',
+            products,
+            styles: [
+                'index.css'
+            ]
+        })
+    }
+    catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
+        throw (err)
+
+    }
+
+})
+
 
 
 module.exports = router
