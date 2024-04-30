4a7377256e77c26931cef7284490f955016f915c
ciro <giorginiciro@gmail.com>
2024-04-30 20:49:52 -0300

    Ignore github.private.js file in src/config

--- src/config/github.private.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    appId: 888748,
-    clientID: 'Iv1.91d34610c1a48a6f',
-    clientSecret: 'e3f70e859dfe6dff851c9fffdb3d46cd1a1911b9',
-    callbackURL: 'http://localhost:8080/api/sessions/githubcallback',
-}
