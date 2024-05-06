b244206e1fc0b2d3a4b30fdfebb88b01b42cbf61
tuco <giorginiciro@gmail.com>
2024-05-06 11:34:57 -0300

    correccion tutor

---
+++ src/config/github.private.js
@@ -0,0 +1,6 @@
+module.exports = {
+    appId: '888748',
+    clientID: 'Iv1.91d34610c1a48a6f',
+    clientSecret: 'bff73854a6ecf4e8e17c8ffa0ad5f4594c79f2aa',
+    callbackUrl: 'http://localhost:8080/api/sessions/githubcallback'
+}
--- src/routes/realTimeProducts.js
+++ src/routes/realTimeProducts.js
@@ -47,12 +47,12 @@ server.delete('/:pid', async (req, res) => {
         const productId = parseInt(req.params.pid);
         // Eliminar el producto del archivo
         await productManager.removeProduct(productId);
-        // Obtener la lista de productos del archivo actualizada
+        
         const products = await productManager.getProducts();
-        const io = req.app.get('io'); // Acceder al objeto io desde Express
-        // Emitir un evento WebSocket para actualizar el feed de productos en los clientes
+        const io = req.app.get('io'); 
+
         io.emit('updateFeed', products);
-        // Redireccionar al usuario a la página de productos en tiempo real
+
         res.redirect('/realTimeProducts');
     } catch (error) {
         console.error(error);
